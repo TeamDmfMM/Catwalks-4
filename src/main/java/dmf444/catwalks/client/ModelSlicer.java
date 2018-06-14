d1d45be85bd610047afacc660e9dd3b7319e52e4
mincrmatt12 <matthewmirvish@hotmail.com>
2018-06-13 20:56:43 -0400

    do dmfs crap

--- src/main/java/dmf444/catwalks/client/ModelSlicer.java
+++ src/main/java/dmf444/catwalks/client/ModelSlicer.java
@@ -11,6 +11,10 @@ import java.util.function.Function;
 
 public class ModelSlicer {
 
+    public List<BakedQuad> slice(List<BakedQuad> quads, AxisAlignedBB bb, Vec3d offset) {
+        return this.slice(quads, bb, offset, (BakedQuad q) -> true);
+    }
+
     public List<BakedQuad> slice(List<BakedQuad> quads, AxisAlignedBB bb, Vec3d offset, Function<BakedQuad, Boolean> filter){
         ImmutableList.Builder<BakedQuad> builder = new ImmutableList.Builder<BakedQuad>();
         sliceInto(builder, quads, bb, offset, filter);
@@ -19,7 +23,14 @@ public class ModelSlicer {
     }
 
     private void sliceInto(ImmutableList.Builder<BakedQuad> builder, List<BakedQuad> quads, AxisAlignedBB bb, Vec3d offset, Function<BakedQuad, Boolean> filter) {
+        double tiny = 0.000001;
+        AxisAlignedBB expandedBB = bb.expand(tiny, tiny, tiny);
+        for (BakedQuad it : quads) {
+            if (!filter.apply(it)) continue;
+            BakedQuad slicedDicedAndServedWithRice = sliceQuad(it, expandedBB, offset);
 
+            if (slicedDicedAndServedWithRice != null) builder.add(slicedDicedAndServedWithRice);
+        }
     }
 
     private BakedQuad sliceQuad(BakedQuad quad, AxisAlignedBB bb, Vec3d offset){
