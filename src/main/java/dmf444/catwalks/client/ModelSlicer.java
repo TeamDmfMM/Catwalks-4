61d0b5797d2a616a1db82939ae32c9e290cea143
dmf444 <dmf444@hotmail.com>
2018-06-13 21:00:09 -0400

    broken model slicer incoming!

--- src/main/java/dmf444/catwalks/client/ModelSlicer.java
+++ src/main/java/dmf444/catwalks/client/ModelSlicer.java
@@ -2,8 +2,12 @@ package dmf444.catwalks.client;
 
 import com.google.common.collect.ImmutableList;
 import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.vertex.VertexFormatElement;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.Vec3d;
+import net.minecraftforge.client.model.pipeline.UnpackedBakedQuad;
+import net.minecraftforge.client.model.pipeline.VertexTransformer;
+import scala.actors.threadpool.Arrays;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,6 +38,25 @@ public class ModelSlicer {
     }
 
     private BakedQuad sliceQuad(BakedQuad quad, AxisAlignedBB bb, Vec3d offset){
-        positions = ArrayList
+        ArrayList<Vec3d> positions = new ArrayList<Vec3d>();
+        positions.add(Vec3d.ZERO);
+        positions.add(Vec3d.ZERO);
+        positions.add(Vec3d.ZERO);
+        positions.add(Vec3d.ZERO);
+
+        int i = 0;
+        UnpackedBakedQuad.Builder builder = new UnpackedBakedQuad.Builder(quad.getFormat());
+
+        quad.pipe(new VertexTransformer(builder){
+            @Override
+            public void put(int element, float... data)
+            {
+                VertexFormatElement.EnumUsage usage = parent.getVertexFormat().getElement(element).getUsage();
+                if(usage == VertexFormatElement.EnumUsage.POSITION && data.length == 3){
+                    positions.add(i++, new Vec3d(data[0], data[1], data[2]));
+                }
+                super.put(element, data);
+            }
+        });
     }
 }
